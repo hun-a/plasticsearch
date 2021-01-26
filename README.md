9ebdc86fec82ba906c2772154c548150ce8eb02b
hun-a <seunghun.chan@gmail.com>
2021-01-27 03:03:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Plasticsearch
+
+## 의미
+- plastic: 온순한, 모양을 이루는, 성형의
+- search: 검색
+- 사실은 [elasticsearch](https://www.elastic.co/elasticsearch/) 모조품
+
+## 정의
+- [Apache Lucene](https://lucene.apache.org/) 기반의 **매우 간단**하게 구현된 검색 API 서버
---
+++ src/main/java/io/huna/plasticsearch/PlasticsearchApplication.java
@@ -0,0 +1,12 @@
+package io.huna.plasticsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlasticsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlasticsearchApplication.class, args);
+    }
+}
