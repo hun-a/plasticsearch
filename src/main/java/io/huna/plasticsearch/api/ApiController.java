4a82f8e7b8d65ed1aeb21467e19516a9af7ace39
hun-a <seunghun.chan@gmail.com>
2021-01-28 02:39:32 +0900

    검색 기능 구현 중... 졸리군

--- src/main/java/io/huna/plasticsearch/api/ApiController.java
+++ src/main/java/io/huna/plasticsearch/api/ApiController.java
@@ -1,10 +1,13 @@
 package io.huna.plasticsearch.api;
 
 import io.huna.plasticsearch.engine.index.Indexer;
+import io.huna.plasticsearch.engine.search.Searcher;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiController {
 
     private final Indexer indexer;
+
+    private final Searcher searcher;
 
     @PostMapping(value = "/{name}")
     public String createIndex(@PathVariable(value = "name") String indexName) throws Exception {
@@ -22,5 +27,10 @@ public class ApiController {
     @PostMapping(value = "/{name}/{id}")
     public String createDocument(@PathVariable(value = "name") String indexName, @PathVariable("id") String documentId, @RequestBody String body) throws Exception {
         return indexer.createDocument(indexName, documentId, body);
+    }
+
+    @GetMapping(value = "/_search")
+    public String search(@RequestParam(value = "q") String q, @RequestParam(value = "field") String field) {
+        return searcher.search(q, field);
     }
 }
---
+++ src/main/java/io/huna/plasticsearch/engine/search/Searcher.java
@@ -0,0 +1,14 @@
+package io.huna.plasticsearch.engine.search;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class Searcher {
+
+    public String search(String q, String field) {
+        // parse the q and field
+        return null;
+    }
+}
